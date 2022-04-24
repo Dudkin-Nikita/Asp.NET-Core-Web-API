9a4d9636a08aac5ea0a724430160b71ca670edc2
DESKTOP-UUPP9C7\user <nikd926@gmail.com>
2022-04-24 20:32:19 +0300

    Add migrations and CRUD operations with meetups

---
+++ Meetups/Meetups/Controllers/MeetupController.cs
@@ -0,0 +1,83 @@
+﻿using Meetups.Domain;
+using Meetups.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Meetups.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeetupController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public MeetupController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Meetup>>> Get()
+        {
+            return Ok(await context.Meetups.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Meetup>> Get(int id)
+        {
+            var meetup = await context.Meetups.FindAsync(id);
+            if (meetup == null)
+            {
+                return BadRequest("Meetup not found");
+            }
+            return Ok(meetup);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Meetup>>> AddHero(Meetup meetup)
+        {
+            context.Meetups.Add(meetup);
+            await context.SaveChangesAsync();
+            return Ok(await context.Meetups.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Meetup>>> UpdateMeetup(Meetup request)
+        {
+            var meetup = await context.Meetups.FindAsync(request.Id);
+            if (meetup == null)
+            {
+                return BadRequest("Meetup not found");
+            }
+            meetup.Name = request.Name;
+            meetup.Description = request.Description;
+            meetup.Place = request.Place;
+            meetup.Date = request.Date;
+
+            //context.Entry(entity).State = EntityState.Modified;
+
+            await context.SaveChangesAsync();
+
+            return Ok(await context.Meetups.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Meetup>> Delete(int id)
+        {
+            var hero = await context.Meetups.FindAsync(id);
+            if (hero == null)
+            {
+                return BadRequest("Meetup not found");
+            }
+
+            context.Meetups.Remove(hero);
+
+            await context.SaveChangesAsync();
+
+            return Ok(await context.Meetups.ToListAsync());
+        }
+
+
+
+    }
+}
---
+++ Meetups/Meetups/Migrations/20220424170307_CreateInitial.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Meetups.Migrations
+{
+    public partial class CreateInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meetups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Place = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meetups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Keywords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    MeetupId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Keywords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Keywords_Meetups_MeetupId",
+                        column: x => x.MeetupId,
+                        principalTable: "Meetups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Keywords_MeetupId",
+                table: "Keywords",
+                column: "MeetupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Keywords");
+
+            migrationBuilder.DropTable(
+                name: "Meetups");
+        }
+    }
+}
